2bd097a73a13aa6639f9e10f6981894c95c5b930
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-25 08:21:45 +0400

    file update grant test access

--- infra.php
+++ infra.php
@@ -7,6 +7,9 @@ if(isset($_GET['-config'])&&$_GET['-config']=='update'){
 $path = Config::get('path');
 if ($path['fs']&&is_file($path['data'].'update')) {
 	unlink($path['data'].'update');
+	Config::get('access');
+	//Файл update даёт право на тестирование сайта
+	Config::$conf['access']['test']=true;
 	if (!Config::$update) {
 		header('Infrajs-Path-Update:true');
 		Path::fullrmdir($path['cache']);
