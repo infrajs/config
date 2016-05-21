71be8c9c7896110a9aca47b8b3476b96fa992a19
Anton <a.kiyatkin@itlf.ru>
2016-05-21 21:04:14 +0400

    config from js file

---
+++ js.php
@@ -0,0 +1,18 @@
+<?php
+use infrajs\load\Load;
+use infrajs\config\Config;
+use infrajs\router\Router;
+use infrajs\ans\Ans;
+
+if (!is_file('vendor/autoload.php')) {
+	chdir('../../../');
+	require_once('vendor/autoload.php');
+	Router::init();
+}
+
+
+$js = 'if (!window.infra) window.infra={}; if (!window.infrajs) window.infrajs={}; infra.conf=('.Load::json_encode(Config::pub()).'); infra.config=function(name){ if(!name)return infra.conf; return infra.conf[name] };';
+$js .= 'window.Config = {}; Config.get = infra.config; Config.conf = infra.conf;';
+
+
+return Ans::js($js);
