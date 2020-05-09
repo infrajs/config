357d7eb05bb7285ebf697c21c07378f052d499a6
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-09 04:48:37 +0400

    fix

--- js.php
+++ js.php
@@ -4,7 +4,7 @@ use infrajs\load\Load;
 use infrajs\config\Config;
 use infrajs\ans\Ans;
 
-$js = 'if (!window.infra) window.infra={}; if (!window.infrajs) window.infrajs={}; infra.conf=(' . Load::json_encode(Config::pub()) . '); ';
+$js = 'if (!window.infra) window.infra={}; infra.conf=(' . Load::json_encode(Config::pub()) . '); ';
 $js .= 'infra.config = function (name){ if(!name)return infra.conf; return infra.conf[name]; };';
 $js .= 'window.Config = {}; Config.get = infra.config; Config.conf = infra.conf; export {Config}';
 
